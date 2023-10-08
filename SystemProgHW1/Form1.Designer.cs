72e04b9f4a14fea7b5daa274fd5cc15f7dd1e93a
Gerg_hn60 <Gerg_hn60@student.itstep.org>
2023-10-09 01:44:34 +0200

    Add project files.

---
+++ SystemProgHW1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SystemProgHW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Franklin Gothic Heavy", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(239, 139);
+            button1.Name = "button1";
+            button1.Size = new Size(300, 59);
+            button1.TabIndex = 0;
+            button1.Text = "Report 12B-L3-PEAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btn_Message;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._12B_L3;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(212, 186);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Franklin Gothic Medium Cond", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(239, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(255, 30);
+            label1.TabIndex = 2;
+            label1.Text = "NAME: 12B-L3 (\"Yaroslav\")";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Franklin Gothic Medium Cond", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(239, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(312, 30);
+            label2.TabIndex = 3;
+            label2.Text = "CONTAINMENT: Section 3 \"PEAR\"";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Franklin Gothic Medium Cond", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(239, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(206, 30);
+            label3.TabIndex = 4;
+            label3.Text = "ACCESS LEVEL: Intern";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 211);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "12B-L3";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ SystemProgHW1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+
+//Howdy :)
+//This is an attemp to combine all of the assignments into one semi-coherent application
+//I really liked working on this one :)
+//Although most of the 'work' was figuring out win32.dll
+//Anyway it was fun
+
+
+
+
+
+namespace SystemProgHW1
+{
+
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint RegisterWindowMessage(string lpString);
+
+        [DllImport("User32.dll")]
+        static extern Boolean MessageBeep(UInt32 beepType);
+
+
+
+
+        private uint _messageId = RegisterWindowMessage("BreakAccessLock");
+        private Random _rand = new Random();
+
+        
+
+        bool _accessLock = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void btn_Message(object sender, EventArgs e)
+        {
+            if (_accessLock)
+            {
+                MessageBox(IntPtr.Zero, "SUBJECT NAME: Yaroslav\nSUBJECT APPEARENCE: Humanoid\nDANGER LEVEL: Unknown", "REPORT 12B-L3-PEAR", 0);
+                MessageBox(IntPtr.Zero, "FOUND: \nSubject was found inside of an appartment building on ☐☐☐☐☐☐ avn. 126 ☐☐☐☐☐, ☐☐☐☐☐☐☐☐. During the opertion to capture the subject, 12B-L3 was consuming an unkown liquid(later specified to be \"tea\".) ", "REPORT 12B-L3-PEAR: FOUND", 0);
+                MessageBox(IntPtr.Zero, "DESCRIPTION: \n12B-L3 is a 16 year-old male, who has an appearance of a typical homo sapiens teen. There is nothing unusual in the subjects appearance.\n12B-L3 prefers to be adressed as 'Yaroslav' and has a mental capacity of a human teen. It was observed that 12B-L3 feels uncomforable when observed 24/7.", "REPORT 12B-L3-PEAR: DESCRIPTION", 0);
+            }
+            else
+            {
+                MessageBox(IntPtr.Zero, "SUBJECT NAME: Yaroslav\nSUBJECT APPEARENCE: Huanmoid\nDANGER LEVEL: Unknown", "REPORT 12B-L3-PEAR: YOU AR", 0);
+                MessageBox(IntPtr.Zero, "FOUND: \nBtcesju aws dofnu idsnei fo na trnapempta ugiibndl no ☐☐☐☐☐☐ nva. 126 ☐☐☐☐☐, ☐☐☐☐☐☐☐☐. Uirndg hte opoirtne ot eutracp teh cusbtje, 12B-L3 aws ominngcus na oknwun ldiqiu(tealr espfeidci ot eb eta.) ", "REPORT 12B-L3-PEAR: WHY I", 0);
+                MessageBox(IntPtr.Zero, "DESCRIPTION: \n12B-L3 si a 16 reya-dol eaml, ohw ash na aenpaarcep fo a paltcyi moho saenspi etne. Ereth si nithngo uansuul ni het ejbcstsu ecappaanre.\n12B-L3 ersrpfe ot eb ersdsead sa 'Rsyovaal' dan sha a tnamle cpicayat fo a auhnm ente. Ti wsa bsevorde ttah 12B-L3 elsef cfraoleounmb wenh eedrbvos 24/7.", "REPORT 12B-L3-PEAR: RNSICTOEPID", 0);
+            }
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == _messageId)
+            {
+                _accessLock = false;
+                pictureBox1.Image = SystemProgHW1.Properties.Resources._12B_L3_Brocken;
+                label2.Text = "CONTAINMENT: Breached containment";
+                label3.Text = "ACESS LEVEL: Level 4 Professor";
+                timer1.Enabled = true;
+                this.BackColor = Color.Black;
+                label1.ForeColor = Color.WhiteSmoke;
+                label2.ForeColor = Color.WhiteSmoke;
+                label3.ForeColor = Color.WhiteSmoke;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBeep(0);
+            timer1.Interval = _rand.Next(700, 1500);
+        }
+    }
+}
